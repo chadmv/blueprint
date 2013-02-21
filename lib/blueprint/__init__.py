3476cf744f51ffd61418dc75d3e7aaa1b93be906
Matt Chambers <mvchambers@me.com>
2013-02-21 23:34:56 +1300

    Init commit from plow code, nothing probably works.

---
+++ lib/blueprint/__init__.py
@@ -0,0 +1,9 @@
+
+from app import PluginManager, Application, BlueprintRunner
+
+from job import Job
+from layer import Layer, Task, SetupTask, DependType, Depend
+
+
+PluginManager.loadAllPlugins()
+
