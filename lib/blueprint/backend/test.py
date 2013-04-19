93b9cadba3454b0b4f5fc0f282233a661ef00aa2
Matt Chambers <mvchambers@me.com>
2013-04-20 09:50:45 +1200

    Update test module to new API.

--- lib/blueprint/backend/test.py
+++ lib/blueprint/backend/test.py
@@ -1,7 +1,7 @@
 """A simple test backend that can be used to manually execute tasks."""
 
-def launch(runner):
-    """
-    Main entry point to launch plugin.
-    """
-    runner.setup()
+def serialize(runner):
+    pass
+
+def launch(runner, spec):
+    pass
