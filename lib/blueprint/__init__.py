9d5302c5e301fea71107a1836f3378556f73a8b6
sqlboy <mvchambers@me.com>
2013-06-27 10:17:31 +1200

    Add PostTask to exported entries.

--- lib/blueprint/__init__.py
+++ lib/blueprint/__init__.py
@@ -1,6 +1,6 @@
 
 from app import PluginManager, Application, BlueprintRunner, loadScript
-from layer import Layer, Task, TaskContainer, TaskIterator, SetupTask, DependType, Depend
+from layer import Layer, Task, PostTask, TaskContainer, TaskIterator, SetupTask, DependType, Depend
 from job import Job
 
 PluginManager.loadAllPlugins()
